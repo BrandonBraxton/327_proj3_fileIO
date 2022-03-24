6730339c4dae6d71b9723b97180aeb4d55aafb56
BrandonBraxton <brandonbraxton28@gmail.com>
2022-03-24 16:35:54 -0400

    pj3

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,19 +6,64 @@
  */
 #include <iostream>
 #include <fstream>
+//#include<vector>
+
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
+vector<string> v;
 
+	//trys to open filename, and return its contents in contents
+	//it will strip out only \n's (char return\line feed)
+	//returns:
+	//SUCCESS all worked, contents string contains all in the file
+	//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream openFile;
+	openFile.open(filename.c_str());
+
+	if(!openFile.is_open())
+	  {
+	    return COULD_NOT_OPEN_FILE_TO_READ;
+	  }
+
+		while(!openFile.eof()){
+			string line;
+			getline(openFile, line);
+			contents += line;
+		}
+
+		openFile.close();
+
+	return SUCCESS;
+
 }
 
+//trys to open filename, and serialize  myEntryVector contents to it
+//returns:
+//SUCCESS all worked, filename contains myEntryVector strings
+//COULD_NOT_OPEN_FILE_TO_WRITE
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream outputfile;
+	outputfile.open(filename.c_str());
+
+	if (!outputfile.is_open())
+	{
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+	for (v = myEntryVector.begin(); v != myEntryVector.end(); v++)
+	{
+		outputfile << *v;
+	}
+
+	outputfile.close();
+
+	return SUCCESS;
+
+
 }
 
 
